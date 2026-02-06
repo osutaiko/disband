f235cff64911f9c7d7549b8058b00023d26e5a59
Dongheon Han <henongod@gmail.com>
2026-02-06 15:20:51 +0900

    feat: account for tempo and time signature changes in audio analysis markers

--- src/features/session/useAudioAnalysisMarkers.ts
+++ src/features/session/useAudioAnalysisMarkers.ts
@@ -4,28 +4,34 @@ export const useAudioAnalysisMarkers = (api, selectedTrackId, endMs) => {
   return useMemo(() => {
     if (!api?.score) return [];
     const currentTrack = api.score.tracks[selectedTrackId ?? 0];
-    const noteMarkers: { timestamp: number; length: number }[] = [];
 
+    const noteMarkers: { timestamp: number; length: number }[] = [];
     const barMarkers: { index: number, timestamp: number }[] = [];
     const quarterBarMarkers: { timestamp: number }[] = [];
-    
-    // TODO: Account for tempo changes
-    let currentTempo = api.score.tempo;
+
     const PPQ = 960;
-    const msPerTick = 60000 / (currentTempo * PPQ);
-
-    let barIndex = 1;
-    let tick = 0;
-
-    const beatsPerBar = api.score.timeSignature?.beats ?? 4;
-    const beatUnit = api.score.timeSignature?.beatType ?? 4;
-    const ticksPerBeat = PPQ * (4 / beatUnit);
-    const ticksPerBar = ticksPerBeat * beatsPerBar;
-    const endTick = endMs / msPerTick;
     
+    let currentBpm = api.score.tempo ?? 120;
+    let msPerTick = 60000 / (currentBpm * PPQ);
+    let currentBarStartMs = 0;
+
     // Note markers
     currentTrack.staves.forEach((staff) => {
       staff.bars.forEach((bar) => {
+        bar.masterBar.tempoAutomations.forEach((tempoAutomation) => {{
+          currentBpm = tempoAutomation.value;
+          msPerTick = 60000 / (currentBpm * PPQ);
+        }});
+
+        // Bar markers
+        barMarkers.push({
+          index: bar.masterBar.index + 1,
+          timestamp: currentBarStartMs,
+        });
+
+        // Quarter bar markers
+        // TODO
+
         bar.voices.forEach((voice) => {
           voice.beats.forEach((beat) => {
             if (!beat.isRest) { 
@@ -36,32 +42,12 @@ export const useAudioAnalysisMarkers = (api, selectedTrackId, endMs) => {
             }
           });
         });
+        
+        const quarterNotesPerBar = (bar.masterBar.timeSignatureNumerator / bar.masterBar.timeSignatureDenominator * 4)
+        currentBarStartMs += quarterNotesPerBar * PPQ * msPerTick;
       });
     });
 
-    while (tick <= endTick) {
-      const timeMs = tick * msPerTick;
-
-      // Bar marker
-      barMarkers.push({
-        index: barIndex,
-        timestamp: timeMs,
-      });
-
-      // Quarter bar marker
-      for (let b = 1; b < beatsPerBar; b++) {
-        const beatTick = tick + b * ticksPerBeat;
-        if (beatTick >= endTick) break;
-
-        quarterBarMarkers.push({
-          timestamp: beatTick * msPerTick,
-        });
-      }
-
-      tick += ticksPerBar;
-      barIndex++;
-    }
-    
     return { noteMarkers, barMarkers, quarterBarMarkers };
   }, [api, selectedTrackId]);
 };
